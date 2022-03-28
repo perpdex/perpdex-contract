334f41b566ff0b433a427b5302a4759104a8b3c8
a <a@example.com>
2022-03-28 21:27:08 +0900

    fixed uniswap v2 to compile with solidity 0.7.6.

--- contracts/amm/uniswap_v2/test/ERC20.sol
+++ contracts/amm/uniswap_v2/test/ERC20.sol
@@ -1,9 +1,10 @@
-pragma solidity =0.5.16;
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.7.6;
 
-import '../UniswapV2ERC20.sol';
+import "../UniswapV2ERC20.sol";
 
 contract ERC20 is UniswapV2ERC20 {
-    constructor(uint _totalSupply) public {
+    constructor(uint256 _totalSupply) public {
         _mint(msg.sender, _totalSupply);
     }
 }
