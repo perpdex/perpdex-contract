2be92204c4b026945c34a9296bb070c3037d118b
a <a@example.com>
2022-04-05 04:32:07 +0900

    update Uniswap v2 pairCodeHash

--- contracts/amm/uniswap_v2_periphery/libraries/UniswapV2Library.sol
+++ contracts/amm/uniswap_v2_periphery/libraries/UniswapV2Library.sol
@@ -29,7 +29,7 @@ library UniswapV2Library {
                         hex"ff",
                         factory,
                         keccak256(abi.encodePacked(token0, token1)),
-                        hex"96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f" // init code hash
+                        hex"79d82be0aced89602c8129463f326cf31812ec3b0e0d82e9501004cd1ca2f3ba" // init code hash
                     )
                 )
             )
