06db09a84b9ee84e993905fb6a5fce5597b30f71
a <a@example.com>
2022-04-08 07:22:34 +0900

    wait contract deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -45,6 +45,13 @@ const config = {
             ethUsd: "0x0715A7794a1dc8e42615F059dD6e406A6594651A",
         },
     },
+    fuji: {
+        weth: "0xd00ae08403B9bbb9124bB305C09058E32C39A48c", // WAVAX
+        usdc: "0x5425890298aed601595a70AB815c96711a31Bc65",
+        chainlinkDataFeed: {
+            ethUsd: "0x5498BB86BC934c8D34FDA08E81D444153d0D06aD", // AVAX/USD
+        },
+    },
 }[hre.network.name]
 
 async function main() {
@@ -53,52 +60,63 @@ async function main() {
         config.chainlinkDataFeed.ethUsd,
         15 * 60,
     )) as ChainlinkPriceFeed
+    await chainlinkPriceFeed.deployed()
 
     const quoteTokenFactory = await ethers.getContractFactory("QuoteToken")
     const quoteToken = (await quoteTokenFactory.deploy()) as QuoteToken
+    await quoteToken.deployed()
     await quoteToken.initialize(quoteTokenName, quoteTokenSymbol)
 
     const baseTokenFactory = await ethers.getContractFactory("BaseToken")
     const baseToken = (await baseTokenFactory.deploy()) as BaseToken
+    await baseToken.deployed()
     await baseToken.initialize(baseTokenName, baseTokenSymbol, chainlinkPriceFeed.address)
 
     const factoryFactory = await ethers.getContractFactory("UniswapV2Factory")
     const uniV2Factory = (await factoryFactory.deploy(ethers.constants.AddressZero)) as UniswapV2Factory
+    await uniV2Factory.deployed()
     console.log("uniV2Factory " + uniV2Factory.address)
 
     const clearingHouseConfigFactory = await ethers.getContractFactory("ClearingHouseConfig")
     const clearingHouseConfig = (await clearingHouseConfigFactory.deploy()) as ClearingHouseConfig
+    await clearingHouseConfig.deployed()
     await clearingHouseConfig.initialize()
     console.log("clearingHouseConfig " + clearingHouseConfig.address)
 
     const marketRegistryFactory = await ethers.getContractFactory("MarketRegistryPerpdex")
     const marketRegistry = (await marketRegistryFactory.deploy()) as MarketRegistryPerpdex
+    await marketRegistry.deployed()
     await marketRegistry.initialize(uniV2Factory.address, quoteToken.address)
     console.log("marketRegistry " + marketRegistry.address)
 
     const orderBookFactory = await ethers.getContractFactory("OrderBookUniswapV2")
     const orderBook = (await orderBookFactory.deploy()) as OrderBookUniswapV2
+    await orderBook.deployed()
     await orderBook.initialize(marketRegistry.address)
     console.log("orderBook " + orderBook.address)
 
     const accountBalanceFactory = await ethers.getContractFactory("AccountBalancePerpdex")
     const accountBalance = (await accountBalanceFactory.deploy()) as AccountBalancePerpdex
+    await accountBalance.deployed()
     await accountBalance.initialize(clearingHouseConfig.address, orderBook.address)
     console.log("accountBalance " + accountBalance.address)
 
     const exchangeFactory = await ethers.getContractFactory("ExchangePerpdex")
     const exchange = (await exchangeFactory.deploy()) as ExchangePerpdex
+    await exchange.deployed()
     await exchange.initialize(marketRegistry.address, orderBook.address, clearingHouseConfig.address)
     await orderBook.setExchange(exchange.address)
     console.log("exchange " + exchange.address)
 
     const insuranceFundFactory = await ethers.getContractFactory("InsuranceFund")
     const insuranceFund = (await insuranceFundFactory.deploy()) as InsuranceFund
+    await insuranceFund.deployed()
     await insuranceFund.initialize(config.usdc)
     console.log("insuranceFund " + insuranceFund.address)
 
     const vaultFactory = await ethers.getContractFactory("Vault")
     const vault = (await vaultFactory.deploy()) as Vault
+    await vault.deployed()
     await vault.initialize(insuranceFund.address, clearingHouseConfig.address, accountBalance.address, exchange.address)
     await insuranceFund.setBorrower(vault.address)
     await accountBalance.setVault(vault.address)
@@ -106,6 +124,7 @@ async function main() {
 
     const clearingHouseFactory = await ethers.getContractFactory("ClearingHousePerpdex")
     const clearingHouse = (await clearingHouseFactory.deploy()) as ClearingHousePerpdex
+    await clearingHouse.deployed()
     await clearingHouse.initialize(
         clearingHouseConfig.address,
         vault.address,
