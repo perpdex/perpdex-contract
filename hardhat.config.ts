a731a0db6132b09be8364ce68c5e6354c1abaad7
a <a@example.com>
2022-04-10 08:33:51 +0900

    update deploy scripts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import "@nomiclabs/hardhat-ethers"
 import "@nomiclabs/hardhat-waffle"
+import "@nomiclabs/hardhat-etherscan"
 import "@openzeppelin/hardhat-upgrades"
 import "@typechain/hardhat"
 import "hardhat-contract-sizer"
@@ -26,6 +27,15 @@ const config: HardhatUserConfig = {
     networks: {
         hardhat: {
             allowUnlimitedContractSize: true,
+        },
+    },
+    etherscan: {
+        apiKey: {
+            mainnet: process.env.ETHERSCAN_API_KEY,
+            ropsten: process.env.ETHERSCAN_API_KEY,
+            rinkeby: process.env.ETHERSCAN_API_KEY,
+            polygonMumbai: process.env.POLYGONSCAN_API_KEY,
+            polygon: process.env.POLYGONSCAN_API_KEY,
         },
     },
     dependencyCompiler: {
@@ -59,17 +69,20 @@ if (process.env.TESTNET_PRIVATE_KEY) {
         config.networks.rinkeby = {
             url: "https://rinkeby.infura.io/v3/" + process.env.INFURA_PROJECT_ID,
             accounts: [process.env.TESTNET_PRIVATE_KEY],
+            gasMultiplier: 2,
         }
     }
 
     config.networks.mumbai = {
         url: "https://rpc-mumbai.maticvigil.com",
         accounts: [process.env.TESTNET_PRIVATE_KEY],
+        gasMultiplier: 2,
     }
 
     config.networks.fuji = {
         url: "https://api.avax-test.network/ext/bc/C/rpc",
         accounts: [process.env.TESTNET_PRIVATE_KEY],
+        gasMultiplier: 2,
     }
 }
 
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -13,46 +13,13 @@ import {
     Vault,
 } from "../typechain"
 import { ChainlinkPriceFeed } from "../typechain/perp-oracle"
-
-const hre = require("hardhat")
+import { config, safeVerify } from "./common"
 
 const quoteTokenName = "QuoteToken"
 const quoteTokenSymbol = "QUOTE"
 
 const baseTokenName = "BaseTokenEth"
 const baseTokenSymbol = "BASEETH"
-
-const config = {
-    localhost: {
-        // eth mainnet fork for test
-        weth: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-        usdc: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-        chainlinkDataFeed: {
-            ethUsd: "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419",
-        },
-    },
-    rinkeby: {
-        weth: "0xc778417E063141139Fce010982780140Aa0cD5Ab",
-        usdc: "0xeb8f08a975Ab53E34D8a0330E0D34de942C95926",
-        chainlinkDataFeed: {
-            ethUsd: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
-        },
-    },
-    mumbai: {
-        weth: "0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa",
-        usdc: "0xe11A86849d99F524cAC3E7A0Ec1241828e332C62",
-        chainlinkDataFeed: {
-            ethUsd: "0x0715A7794a1dc8e42615F059dD6e406A6594651A",
-        },
-    },
-    fuji: {
-        weth: "0xd00ae08403B9bbb9124bB305C09058E32C39A48c", // WAVAX
-        usdc: "0x5425890298aed601595a70AB815c96711a31Bc65",
-        chainlinkDataFeed: {
-            ethUsd: "0x5498BB86BC934c8D34FDA08E81D444153d0D06aD", // AVAX/USD
-        },
-    },
-}[hre.network.name]
 
 async function main() {
     console.log(config)
@@ -63,6 +30,7 @@ async function main() {
         15 * 60,
     )) as ChainlinkPriceFeed
     await chainlinkPriceFeed.deployed()
+    await chainlinkPriceFeed.update()
     console.log("chainlinkPriceFeed " + chainlinkPriceFeed.address)
 
     const quoteTokenFactory = await ethers.getContractFactory("QuoteToken")
@@ -155,6 +123,25 @@ async function main() {
     // TODO: Investigate an issue where setAccountBalance fails only with rinkeby
     console.log("call setAccountBalance")
     await exchange.setAccountBalance(accountBalance.address)
+
+    // TODO: Investigate an issue where setMaxPriceRocWithinBlock fails only with rinkeby
+    console.log("call setMaxPriceRocWithinBlock")
+    await exchange.setMaxPriceRocWithinBlock(baseToken.address, ethers.BigNumber.from(2).pow(95))
+
+    console.log("verify")
+
+    await safeVerify(chainlinkPriceFeed.address, [config.chainlinkDataFeed.ethUsd, 15 * 60])
+    await safeVerify(quoteToken.address, [])
+    await safeVerify(baseToken.address, [])
+    await safeVerify(uniV2Factory.address, [ethers.constants.AddressZero])
+    await safeVerify(clearingHouseConfig.address, [])
+    await safeVerify(marketRegistry.address, [])
+    await safeVerify(orderBook.address, [])
+    await safeVerify(accountBalance.address, [])
+    await safeVerify(exchange.address, [])
+    await safeVerify(insuranceFund.address, [])
+    await safeVerify(vault.address, [])
+    await safeVerify(clearingHouse.address, [])
 
     console.log("deploy finished")
 }
