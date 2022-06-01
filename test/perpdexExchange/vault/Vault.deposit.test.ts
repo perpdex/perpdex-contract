5332398cc2689c52d706af104a642ff74fdd18ce
takamiml <91838137+takami-ml@users.noreply.github.com>
2022-06-01 12:06:09 +0900

    Refactoring deposit test

--- test/perpdexExchange/vault/Vault.deposit.test.ts
+++ test/perpdexExchange/vault/Vault.deposit.test.ts
@@ -1,40 +1,36 @@
-import { MockContract } from "@eth-optimism/smock"
 import { expect } from "chai"
-import { parseEther, parseUnits } from "ethers/lib/utils"
+import { parseUnits } from "ethers/lib/utils"
 import { ethers, waffle } from "hardhat"
 import { PerpdexExchange, TestERC20 } from "../../../typechain"
 import { createPerpdexExchangeFixture } from "../fixtures"
 
 describe("Vault deposit test", () => {
-    const [admin, alice, bob] = waffle.provider.getWallets()
+    const [admin, alice] = waffle.provider.getWallets()
     const loadFixture: ReturnType<typeof waffle.createFixtureLoader> = waffle.createFixtureLoader([admin])
+    let fixture
     let usdc: TestERC20
     let perpdexExchange: PerpdexExchange
     let usdcDecimals: number
-    let fixture
+
+    function parseUsdc(amount: string) {
+        return parseUnits(amount, usdcDecimals)
+    }
 
     beforeEach(async () => {
-        const _fixture = await loadFixture(createPerpdexExchangeFixture())
-        perpdexExchange = _fixture.perpdexExchange
-        usdc = _fixture.USDC
-        fixture = _fixture
-
+        fixture = await loadFixture(createPerpdexExchangeFixture())
+        perpdexExchange = fixture.perpdexExchange
+        usdc = fixture.USDC
         usdcDecimals = await usdc.decimals()
-        const amount = parseUnits("1000", usdcDecimals)
+
+        const amount = parseUsdc("1000")
         await usdc.mint(alice.address, amount)
 
         await usdc.connect(alice).approve(perpdexExchange.address, ethers.constants.MaxUint256)
     })
 
     describe("settlement token", async () => {
-        let usdcDecimals
-
-        beforeEach(async () => {
-            usdcDecimals = await usdc.decimals()
-        })
-
         it("deposit settlement token", async () => {
-            const amount = parseUnits("100", usdcDecimals)
+            const amount = parseUsdc("100")
 
             // check event has been sent
             await expect(perpdexExchange.connect(alice).deposit(amount))
@@ -42,7 +38,7 @@ describe("Vault deposit test", () => {
                 .withArgs(alice.address, amount)
 
             // reduce alice balance
-            expect(await usdc.balanceOf(alice.address)).to.eq(parseUnits("900", usdcDecimals))
+            expect(await usdc.balanceOf(alice.address)).to.eq(parseUsdc("900"))
 
             // increase vault balance
             expect(await usdc.balanceOf(perpdexExchange.address)).to.eq(amount)
@@ -53,7 +49,7 @@ describe("Vault deposit test", () => {
         })
 
         it("force error, not enough balance", async () => {
-            const amount = parseUnits("1100", await usdc.decimals())
+            const amount = parseUsdc("1100")
             await expect(perpdexExchange.connect(alice).deposit(amount)).to.be.revertedWith(
                 "ERC20: transfer amount exceeds balance",
             )
@@ -61,14 +57,12 @@ describe("Vault deposit test", () => {
 
         it("force error, inconsistent vault balance with deflationary token", async () => {
             usdc.setTransferFeeRatio(50)
-            await expect(perpdexExchange.connect(alice).deposit(parseUnits("100", usdcDecimals))).to.be.revertedWith(
-                "V_IBA",
-            )
+            await expect(perpdexExchange.connect(alice).deposit(parseUsdc("100"))).to.be.revertedWith("V_IBA")
             usdc.setTransferFeeRatio(0)
         })
 
         it("force error, zero amount", async () => {
-            await expect(perpdexExchange.connect(alice).deposit("0")).to.be.revertedWith("V_ZA")
+            await expect(perpdexExchange.connect(alice).deposit(parseUsdc("0"))).to.be.revertedWith("V_ZA")
         })
     })
 })
