1f693df807c98c48461c6792ea6e80ad904ea68d
a <a@example.com>
2022-06-12 19:19:51 +0900

    add zero sum check test

--- contracts/test/TestPerpdexMarket.sol
+++ contracts/test/TestPerpdexMarket.sol
@@ -2,10 +2,20 @@
 pragma solidity 0.7.6;
 pragma abicoder v2;
 
+import { FixedPoint96 } from "@uniswap/v3-core/contracts/libraries/FixedPoint96.sol";
+import { SafeCast } from "@openzeppelin/contracts/utils/SafeCast.sol";
+import { SignedSafeMath } from "@openzeppelin/contracts/math/SignedSafeMath.sol";
 import { PerpdexMarket } from "../PerpdexMarket.sol";
 import { MarketStructs } from "../lib/MarketStructs.sol";
+import { PoolLibrary } from "../lib/PoolLibrary.sol";
+import { PerpMath } from "../lib/PerpMath.sol";
 
 contract TestPerpdexMarket is PerpdexMarket {
+    using PerpMath for int256;
+    using PerpMath for uint256;
+    using SafeCast for uint256;
+    using SignedSafeMath for int256;
+
     constructor(
         string memory symbolArg,
         address exchangeArg,
@@ -27,5 +37,25 @@ contract TestPerpdexMarket is PerpdexMarket {
 
     function setPriceLimitInfo(MarketStructs.PriceLimitInfo memory value) external {
         priceLimitInfo = value;
+    }
+
+    function getLockedLiquidityInfo() external view returns (int256 base, int256 accountValue) {
+        uint256 liquidity = PoolLibrary.MINIMUM_LIQUIDITY;
+
+        if (poolInfo.totalLiquidity == 0) return (0, 0);
+
+        (uint256 poolBase, uint256 poolQuote) = PoolLibrary.getLiquidityValue(poolInfo, liquidity);
+        (int256 delBase, int256 delQuote) =
+            PoolLibrary.getLiquidityDeleveraged(
+                poolInfo.cumBasePerLiquidityX96,
+                poolInfo.cumQuotePerLiquidityX96,
+                liquidity,
+                0,
+                0
+            );
+
+        base = poolBase.toInt256().add(delBase);
+        int256 quote = poolQuote.toInt256().add(delQuote);
+        accountValue = quote.add(base.mulDiv(getShareMarkPriceX96().toInt256(), FixedPoint96.Q96));
     }
 }
--- test/perpdexExchange/integration/complex.test.ts
+++ test/perpdexExchange/integration/complex.test.ts
@@ -4,9 +4,9 @@ import { TestPerpdexExchange, TestPerpdexMarket } from "../../../typechain"
 import { createPerpdexExchangeFixture } from "../fixtures"
 import { BigNumber, Wallet } from "ethers"
 import { getTimestamp, setNextTimestamp } from "../../helper/time"
-import { getCurrentTimestamp } from "hardhat/internal/hardhat-network/provider/utils/getCurrentTimestamp"
+import { MockContract } from "ethereum-waffle"
 
-describe("PerpdexExchange extreme situation", () => {
+describe("PerpdexExchange complex situation", () => {
     let loadFixture = waffle.createFixtureLoader(waffle.provider.getWallets())
     let fixture
 
@@ -18,9 +18,11 @@ describe("PerpdexExchange extreme situation", () => {
     let alice: Wallet
     let bob: Wallet
     let carol: Wallet
+    let priceFeed: MockContract
 
     const PoolAmount = BigNumber.from(10).pow(18)
     const Q96 = BigNumber.from(2).pow(96)
+    const X10_18 = BigNumber.from(10).pow(18)
     const deadline = Q96
     const epsilon = 10
 
@@ -32,6 +34,30 @@ describe("PerpdexExchange extreme situation", () => {
             isExactInput: false,
             amount: amount,
             oppositeAmountBound: BigNumber.from(amount).mul(10),
+            deadline: deadline,
+        })
+    }
+
+    const longLiq = async (trader, liquidator, amount, idx = 0) => {
+        return exchange.connect(liquidator).trade({
+            trader: trader.address,
+            market: markets[idx].address,
+            isBaseToQuote: false,
+            isExactInput: false,
+            amount: amount,
+            oppositeAmountBound: BigNumber.from(amount).mul(10),
+            deadline: deadline,
+        })
+    }
+
+    const shortLiq = async (trader, liquidator, amount, idx = 0) => {
+        return exchange.connect(liquidator).trade({
+            trader: trader.address,
+            market: markets[idx].address,
+            isBaseToQuote: true,
+            isExactInput: true,
+            amount: amount,
+            oppositeAmountBound: 0,
             deadline: deadline,
         })
     }
@@ -125,6 +151,10 @@ describe("PerpdexExchange extreme situation", () => {
         for (let i = 0; i < traders.length; i++) {
             result = result.add(await exchange.getTotalAccountValue(traders[i].address))
         }
+        for (let i = 0; i < markets.length; i++) {
+            const [base, accountValue] = await markets[i].getLockedLiquidityInfo()
+            result = result.add(accountValue)
+        }
         const ethBalance = await hre.ethers.provider.getBalance(exchange.address)
         expect(result).to.be.closeTo(ethBalance, epsilon)
     }
@@ -134,14 +164,19 @@ describe("PerpdexExchange extreme situation", () => {
         for (let i = 0; i < traders.length; i++) {
             result = result.add(await exchange.getPositionShare(traders[i].address, market.address))
         }
-        expect(result).to.be.closeTo(0, epsilon)
+        const [base, accountValue] = await market.getLockedLiquidityInfo()
+        result = result.add(base)
+        expect(result).to.be.closeTo(BigNumber.from(0), epsilon)
     }
 
-    const assertConsistency = async () => {
-        await assertZerosum()
+    const assertBaseZerosum = async () => {
         for (let i = 0; i < markets.length; i++) {
             await assertBaseZerosumByMarket(markets[i])
         }
+    }
+
+    const setIndexPrice = async price => {
+        await priceFeed.mock.getPrice.returns(price)
     }
 
     const setElapsed = async (sec, mine = false) => {
@@ -166,7 +201,7 @@ describe("PerpdexExchange extreme situation", () => {
         fixture = await loadFixture(
             createPerpdexExchangeFixture({
                 isMarketAllowed: true,
-                initPool: true,
+                initPool: false,
             }),
         )
         exchange = fixture.perpdexExchange
@@ -177,6 +212,7 @@ describe("PerpdexExchange extreme situation", () => {
         carol = fixture.carol
         markets = fixture.perpdexMarkets
         traders = [alice, bob, carol]
+        priceFeed = fixture.priceFeed
     })
 
     describe("price limit", () => {
@@ -192,13 +228,14 @@ describe("PerpdexExchange extreme situation", () => {
             await deposit(alice, PoolAmount)
             await deposit(bob, PoolAmount)
             await deposit(carol, 10)
+            await setIndexPrice(X10_18.mul(4))
             await addLiquidity(bob, PoolAmount, PoolAmount.mul(4))
         })
 
         describe("same timestamp", () => {
             describe("long", () => {
                 beforeEach(async () => {
-                    await short(carol, 90) // will be liquidated
+                    await short(carol, 20) // will be liquidated
                     await setElapsed(1, true)
 
                     const amount = await maxLong(alice)
@@ -224,7 +261,7 @@ describe("PerpdexExchange extreme situation", () => {
 
             describe("short", () => {
                 beforeEach(async () => {
-                    await long(carol, 90) // will be liquidated
+                    await long(carol, 20) // will be liquidated
                     await setElapsed(1, true)
 
                     const amount = await maxShort(alice)
@@ -252,7 +289,7 @@ describe("PerpdexExchange extreme situation", () => {
         describe("different timestamp", () => {
             describe("long", () => {
                 beforeEach(async () => {
-                    await short(carol, 90) // will be liquidated
+                    await short(carol, 20) // will be liquidated
                     await setElapsed(1, true)
 
                     const amount = await maxLong(alice)
@@ -264,6 +301,12 @@ describe("PerpdexExchange extreme situation", () => {
                 it("long not limited", async () => {
                     expect(await maxLong(alice)).to.gt(epsilon)
                     expect(await maxLong(bob)).to.gt(epsilon)
+                })
+
+                // TODO:
+                it("long limited by ema", async () => {
+                    // expect(await maxLong(alice)).to.lt(epsilon)
+                    // expect(await maxLong(bob)).to.lt(epsilon)
                 })
 
                 it("short not limited", async () => {
@@ -278,7 +321,7 @@ describe("PerpdexExchange extreme situation", () => {
 
             describe("short", () => {
                 beforeEach(async () => {
-                    await long(carol, 90) // will be liquidated
+                    await long(carol, 20) // will be liquidated
                     await setElapsed(1, true)
 
                     const amount = await maxShort(alice)
@@ -300,6 +343,72 @@ describe("PerpdexExchange extreme situation", () => {
                 it("short liquidation not limited", async () => {
                     expect(await maxShortLiq(carol, alice)).to.gt(epsilon)
                 })
+            })
+        })
+    })
+
+    describe("consistency", () => {
+        beforeEach(async () => {
+            await exchange.connect(owner).setProtocolFeeRatio(1e4)
+            await markets[0].connect(owner).setPriceLimitConfig({
+                normalOrderRatio: 20e4,
+                liquidationRatio: 40e4,
+                emaNormalOrderRatio: 20e4,
+                emaLiquidationRatio: 40e4,
+                emaSec: 300,
+            })
+            await markets[0].connect(owner).setFundingMaxPremiumRatio(5e4)
+            await markets[0].connect(owner).setFundingRolloverSec(3600)
+            await markets[0].connect(owner).setFundingMaxElapsedSec(3600)
+
+            await deposit(alice, PoolAmount)
+            await deposit(bob, PoolAmount)
+            await setIndexPrice(X10_18.mul(4))
+            await addLiquidity(bob, PoolAmount, PoolAmount.mul(4))
+        })
+
+        describe("complex scenario", () => {
+            beforeEach(async () => {
+                const carolSize = await maxLong(carol)
+                let amount = carolSize
+                expect(amount).to.gt(epsilon)
+                await deposit(carol, amount.div(2))
+                await setIndexPrice(X10_18.mul(X10_18))
+                await long(carol, amount) // will be liquidated
+                await setIndexPrice(1)
+                await setElapsed(3600, true)
+
+                for (let i = 0; i < 5; i++) {
+                    amount = await maxShort(alice)
+                    expect(amount).to.gt(epsilon)
+                    await short(alice, amount)
+                    await setIndexPrice(X10_18.mul(X10_18))
+                    await setElapsed(3600, true)
+                }
+
+                await shortLiq(carol, bob, carolSize)
+                await setElapsed(3600, true)
+
+                await removeLiquidity(bob, PoolAmount)
+            })
+
+            it("account value zero sum", async () => {
+                await assertZerosum()
+            })
+
+            it("base zero sum", async () => {
+                await assertBaseZerosum()
+            })
+
+            it("insurance fund profit", async () => {
+                const fundInfo = await exchange.insuranceFundInfo()
+                expect(fundInfo.balance).to.gt(epsilon)
+                expect(fundInfo.liquidationRewardBalance).to.gt(epsilon)
+            })
+
+            it("protocol fee profit", async () => {
+                const protocolFee = await exchange.protocolInfo()
+                expect(protocolFee).to.gt(epsilon)
             })
         })
     })
