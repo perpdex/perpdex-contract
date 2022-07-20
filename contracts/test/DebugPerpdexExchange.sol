affcba5405d14f5ccab883f0d42aecc79a440f23
a <a@example.com>
2022-07-20 12:25:36 +0900

    add zksync2 testnet to DebugPerpdexExchange

--- contracts/test/DebugPerpdexExchange.sol
+++ contracts/test/DebugPerpdexExchange.sol
@@ -7,13 +7,18 @@ import { PerpdexExchange } from "../PerpdexExchange.sol";
 contract DebugPerpdexExchange is PerpdexExchange {
     uint256 private constant _RINKEBY_CHAIN_ID = 4;
     uint256 private constant _SHIBUYA_CHAIN_ID = 81;
+    // https://v2-docs.zksync.io/dev/zksync-v2/temp-limits.html#temporarily-simulated-by-constant-values
+    uint256 private constant _ZKSYNC2_TESTNET_CHAIN_ID = 0;
 
     constructor(address settlementTokenArg) PerpdexExchange(settlementTokenArg) {
         uint256 chainId;
         assembly {
             chainId := chainid()
         }
-        require(chainId == _RINKEBY_CHAIN_ID || chainId == _SHIBUYA_CHAIN_ID, "DPE_C: testnet only");
+        require(
+            chainId == _RINKEBY_CHAIN_ID || chainId == _SHIBUYA_CHAIN_ID || chainId == _ZKSYNC2_TESTNET_CHAIN_ID,
+            "DPE_C: testnet only"
+        );
     }
 
     function setCollateralBalance(address trader, int256 balance) external {
